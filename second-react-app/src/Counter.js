deb27ac492d7767ee2e3f7de881170467e1c14ad
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-08-29 01:52:36 +0530

    second-react-app and learning on state and hooks

---
+++ second-react-app/src/Counter.js
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+let z = 0;
+function Counter() {
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+    return (
+        <>
+        Count Z : {z}
+        <button onClick={() => z = z+1}>Increment</button>
+        <br />
+        Count X : {x}
+        <button onClick={() => setX(x+1)}>Increment</button>
+        <button onClick={() => setX(x-1)}>Decrement</button>
+        <br />
+        Count Y : {y}
+        <button onClick={() => setY(prevY => prevY+1)}>Increment</button>
+        <button onClick={() => setY(prevY => prevY-1)}>Decrement</button>
+        </>
+    )
+}
+
+export default Counter;
+
+/*
+import { useState } from 'react';
+
+function Counter() {
+    const [x, setX] = useState(0);
+
+    const increment = () => {
+        setX(prevX => prevX + 1);
+    };
+
+    const decrement = () => {
+        setX(prevX => prevX - 1);
+    };
+
+    return (
+        <>
+            Count: {x}
+            <button onClick={increment}>Inc</button>
+            <button onClick={decrement}>Dec</button>
+        </>
+    );
+}
+
+export default Counter;
+
+
+*/
