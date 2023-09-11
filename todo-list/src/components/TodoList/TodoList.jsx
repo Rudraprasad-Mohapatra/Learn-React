43aaec38a9bb9e5c4f59b46d4b5d16c05bb0b522
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-09-11 08:56:34 +0530

    removing functional part from ui

--- todo-list/src/components/TodoList/TodoList.jsx
+++ todo-list/src/components/TodoList/TodoList.jsx
@@ -4,7 +4,33 @@ import TodoContext from "../../context/TodoContext";
 import { useContext } from "react";
 // export default function TodoList({ list, updateList }) {
 export default function TodoList() {
-    const { list, setList } = useContext(TodoContext)
+    const { list, setList } = useContext(TodoContext);
+
+    function onFinished(todo, isFinished) {
+        const updatedList = list.map(t => {
+            if (t.id == todo.id) {
+                todo.finished = isFinished
+            }
+            return t;
+        });
+        // updateList(updatedList);
+        setList(updatedList);
+    }
+
+    function onDelete(todo) {
+        const updatedList = list.filter(t => t.id != todo.id)
+        setList(updatedList);
+    }
+
+    function onEdit(todo, todoText) {
+        const updatedList = list.map((t) => {
+            if (t.id == todo.id)
+                todo.todoData = todoText;
+            return t;
+        });
+        setList(updatedList);
+    }
+
     return (
         <div>
             {
@@ -14,32 +40,15 @@ export default function TodoList() {
                     id={todo.id}
                     todoData={todo.todoData}
                     isFinished={todo.finished}
-                    changeFinished={(isFinished) => {
-                        console.log("is finished", isFinished);
-                        const updatedList = list.map(t => {
-                            if (t.id == todo.id) {
-                                todo.finished = isFinished
-                            }
-                            return t;
-                        });
-                        // updateList(updatedList);
-                        setList(updatedList);
-                    }
-                    }
-                    onDelete={() => {
-                        const updatedList = list.filter(t => t.id != todo.id)
-                        setList(updatedList);
-                    }}
-                    onEdit={(todoText)=>{
-                        const updatedList = list.map((t)=>{
-                            if(t.id==todo.id)
-                                todo.todoData = todoText;
-                            return t;
-                        });
-                        setList(updatedList);
+                    changeFinished={(isFinished) => onFinished(todo, isFinished)}
+                    onDelete={() => { onDelete(todo) }}
+                    onEdit={(todoText) => {
+                        onEdit(todo, todoText)
                     }}
                 />)
             }
         </div>
     )
 }
+
+// 06:43
