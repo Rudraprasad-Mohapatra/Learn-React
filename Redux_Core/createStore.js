8c18134413b856e2ebc4ca6c0097d4c710075c1d
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-09-13 11:22:16 +0530

    Implemented Redux Core

--- Redux_Core/createStore.js
+++ Redux_Core/createStore.js
@@ -1,10 +1,11 @@
-import { createStore } from "redux";
+import { createStore, bindActionCreators, combineReducers } from "redux";
 
 const ADD_TODO = "add_todo";
 const DEL_TODO = "delte_todo";
 const UPD_TODO = "edit_todo";
-
-function todoReducer(state, action) {
+const ADD_USER = "add_user";
+// function todoReducer(state, action) { // without combineReducers
+function todoReducer(state = [], action) {
     if (action.type == ADD_TODO) {
         const todoText = action.payload.todoText
         return [
@@ -12,8 +13,8 @@ function todoReducer(state, action) {
             { text: todoText, isFinished: false, id: (state.length == 0) ? 1 : state[state.length - 1].id + 1 }
         ];
     } else if (action.type == DEL_TODO) {
-        const todo = action.payload.todo;
-        return state.filter(t => t.id != todo.id);
+        const todoId = action.payload.todoId;
+        return state.filter(t => t.id != todoId);
     } else if (action.type == UPD_TODO) {
         const todo = action.payload.todo;
         const todoText = action.payload.todoText;
@@ -26,15 +27,60 @@ function todoReducer(state, action) {
     return state;
 }
 
+function userReducr(state = [], action) {
+    const userName = action.payload && action.payload.userName;
+    if (action.type == ADD_USER) {
+        return [
+            ...state, { name: userName, id: (state.length == 0) ? 1 : state[state.length - 1].id + 1 }
+        ]
+    }
+    return state;
+}
+
 // const response = createStore(todoReducer, []);
 
 // console.log(response);
 // console.log(response.getState());
 
-const { dispatch, subscribe, getState, replaceReducer } = createStore(todoReducer, []);
 
-dispatch({ type: ADD_TODO, payload: { todoText: "todo 1" } })
-console.log(getState());
+// action objects -> action creators 
 
-dispatch({ type: ADD_TODO, payload: { todoText: "todo 2" } })
-console.log(getState());
+const addTodo = (todoText) => {
+    return { type: ADD_TODO, payload: { todoText: todoText } }
+}
+const deleteTodo = (id) => {
+    return { type: DEL_TODO, payload: { todoId: id } }
+}
+
+const addUser = (name) => {
+    return { type: ADD_USER, payload: {userName : name}};
+}
+
+// const { dispatch, subscribe, getState, replaceReducer } = createStore(todoReducer, []); without combineReducers
+
+const reducer = combineReducers({ todo: todoReducer, user: userReducr })
+const { dispatch, subscribe, getState, replaceReducer } = createStore(reducer);
+
+// subscribe() will call the function every time there is a state change
+subscribe(() => {
+    console.log(getState());
+})
+
+const actions = bindActionCreators({ addTodo, deleteTodo, addUser }, dispatch); //Binding actions with dispatch
+// const userActions = bindActionCreators({addUser}, dispatch);
+
+// dispatch({ type: ADD_USER, payload: { userName: "Rama" } });
+// dispatch(addUser("Rama"));
+userActions.addUser("Rama");
+
+// dispatch(addTodo("todo 1"))
+// console.log(getState());
+actions.addTodo("todo 1")
+
+// dispatch(addTodo("todo 2"))
+// console.log(getState());
+actions.addTodo("todo 2")
+
+// dispatch(deleteTodo(2));
+// console.log(getState());
+actions.deleteTodo(2)
